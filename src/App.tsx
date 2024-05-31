bea350a6599067ca35ebed34e521b7bbacf31a11
Nikola Grujicic <nikolagrujicic@Nikolas-MacBook-Pro.local>
2024-05-31 11:51:49 +0200

    added basic structure and some libraries

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.scss";
 
 function App() {
   return (
-    <>
-      <h1>En3</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={"wkjnfwenfjwei"} />
+        <Route path="/about" element={"wojnwmedc"} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
