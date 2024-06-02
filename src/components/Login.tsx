84451ac449e3ad04e9c1765c37813007d30794da
Nikola Grujicic <nikolagrujicic@Nikolas-MacBook-Pro.local>
2024-06-02 03:14:30 +0200

    merged

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,12 +1,11 @@
-import React, { useState } from 'react';
-import { ethers } from 'ethers';
-import axios from 'axios';
-import { abi } from '../consts/sc-abi';
-import { bytecode } from '../consts/bytecode';
+import { useState } from "react";
+import { ethers } from "ethers";
+import axios from "axios";
+
 const Login = () => {
   const [account, setAccount] = useState(null);
-  const [message, setMessage] = useState('Sign this message to log in.');
-  const [contractAddress, setContractAddress] = useState('');
+  const [message, setMessage] = useState("Sign this message to log in.");
+  const [contractAddress, setContractAddress] = useState("");
   const [deploying, setDeploying] = useState(false);
 
   const [signature, setSignature] = useState(null);
@@ -66,62 +65,6 @@ const Login = () => {
     }
   };
   console.log(sessionToken);
-
-
-  const deployContract = async () => {
-    try {
-      setDeploying(true);
-
-      // Connect to MetaMask
-      if (!window.ethereum) throw new Error('MetaMask not installed');
-      await window.ethereum.enable();
-      const provider = new ethers.BrowserProvider(window.ethereum);
-      const signer = await provider.getSigner();
-
-      // Create contract instance
-      const factory = new ethers.ContractFactory(abi, bytecode, signer);
-      const contract = await factory.deploy();
-      // Wait for contract to be deployed
-      const status = await contract.waitForDeployment();
-      console.log('contract addresss', contract.target);
-      setContractAddress(contract.target);
-      setTimeout(async () => {
-        const contractInstance = new ethers.Contract(contract.target, abi, signer);
-        const cost = ethers.parseUnits('0.01', 'ether');
-        const result = await contractInstance.createNFTCollection("ayy lmao", "LMAO", "ipfs://QmPPWbX4H9xUcJWnNzKpV1Br5F4DLuv1bDmTQNZy2X5bUE/", 5, cost, "0x142f9DE19A405a1E8B6b71811414110b33998b88", 50);
-        console.log('result', result);
-        
-        // POST  /create-event
-        // Needs header: authentication: JWT 
-        /*{
-          description, 
-          contractAddr
-          name, 
-          startDate, 
-          endDate, 
-          location, 
-          capacity,
-          price,
-          donationAddr,
-          donatationPercentage,
-          image
-        }*/
-        
-        
-        /*setTimeout(async () => {
-          const result = await contractInstance.mintNFT(ethers.toBigInt(0), {value: ethers.parseEther('0.01')});
-          console.log('attytytttsadtytydastytydstysdaty', result);
-          
-        }, 10000);*/
-      }, 20000)
-
-    } catch (error) {
-      console.error('Error deploying contract:', error);
-    } finally {
-      setDeploying(false);
-    }
-  };
-
 
   return (
     <div>
--- src/services/Events/index.ts
+++ src/services/Events/index.ts
@@ -1,22 +1,112 @@
 import axios from "axios";
+import { Addressable } from "ethers";
 
+type EventProps = {
+  name: string;
+  eventDateStart: number;
+  eventDateEnd: number;
+  location: string;
+  description: string;
+  capacity: number;
+  pricePerNftTicket: number;
+  charityPercentage: number;
+  charityWalletAddress: string;
+  contractAddr: string | Addressable;
+  image: string;
+};
 
 const apiUrl = import.meta.env.VITE_API_URL;
 
 export const getEvents = async () => {
-    const response = await axios.get(`${apiUrl}/events`);
-    return response.data;
-}
+  const response = await axios.get(`${apiUrl}/events`);
+  return response.data;
+};
 
 export const getMyEvents = async () => {
+  const sessionToken = localStorage.getItem("session-token");
+
+  if (!sessionToken) {
+    throw new Error("No session token found in localStorage.");
+  }
+  const response = await axios.get(`${apiUrl}/my-events`, {
+    headers: { Authorization: `${sessionToken}` },
+  });
+  return response.data;
+};
+
+// description,
+// contractAddr,
+// name,
+// startDate,
+// endDate,
+// location,
+// capacity,
+// price,
+// donationAddr,
+// donatationPercentage,
+// image,
+
+export const createEvent = async ({
+  name,
+  eventDateStart,
+  eventDateEnd,
+  location,
+  description,
+  capacity,
+  pricePerNftTicket,
+  charityPercentage,
+  charityWalletAddress,
+  contractAddr,
+  image,
+}: EventProps) => {
+  try {
+    const apiUrl = import.meta.env.VITE_API_URL;
     const sessionToken = localStorage.getItem("session-token");
 
     if (!sessionToken) {
       throw new Error("No session token found in localStorage.");
     }
-    const response = await axios.get(`${apiUrl}/my-events`,
-    {
-      headers: { Authorization: `${sessionToken}` },
-    });
-    return response.data;
-}
+
+    //   description,
+    //     contractAddr,
+    //     name,
+    //     startDate,
+    //     endDate,
+    //     location,
+    //     capacity,
+    //     price,
+    //     donationAddr,
+    //     donatationPercentage,
+    //     image,
+
+    const res = await axios.post(
+      `${apiUrl}/create-event`,
+      {
+        description: description,
+        name: name,
+        location: location,
+        capacity: capacity,
+        endDate: eventDateEnd,
+        startDate: eventDateStart,
+        pricePerNftTicket: pricePerNftTicket,
+        charityPercentage: charityPercentage,
+        charityWalletAddress: charityWalletAddress,
+        contractAddr: contractAddr,
+        image: image,
+      },
+      {
+        headers: { Authorization: `${sessionToken}` },
+      }
+    );
+
+    console.log(res);
+    return res;
+
+    console.log("Response:", res.data);
+  } catch (error) {
+    console.error(
+      "Error generating image:",
+      error.response?.data || error.message
+    );
+  }
+};
